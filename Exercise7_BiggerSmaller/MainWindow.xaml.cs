ed0ff344b738c4221ad2e65fa28b5669335ec503
Steplon <43827083+Steplon@users.noreply.github.com>
2019-02-03 15:53:09 +0000

    Game completed

---
+++ Exercise7_BiggerSmaller/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Exercise7_BiggerSmaller
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int x, y;
+        Random rand = new Random();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonStart_Click(object sender, RoutedEventArgs e)
+        {
+            //get first random number
+            x = rand.Next(1, 21);
+            textBlockInitialNumber.Text = "Initial Number:\t" + x.ToString();
+        }
+
+        private void BtnGuess_Click(object sender, RoutedEventArgs e)
+        {
+            //get second random number which is different to x
+            do
+            {
+                y = rand.Next(1, 21);
+            } while (y == x);
+            
+            textBlockSecondNumber.Text = "Second Number:\t" + y.ToString();
+
+            //check which radio selected
+            bool biggerSelected = rbBigger.IsChecked.Equals(true);
+            bool smallerSelected = rbSmaller.IsChecked.Equals(true);
+
+            //result
+            if (biggerSelected && y > x)
+            {
+                textBlockResult.Text = "You Win!!";
+            }
+            else if (smallerSelected && x > y)
+            {
+                textBlockResult.Text = "You Win!!";
+            }
+            else
+            {
+                textBlockResult.Text = "You Lose!!";
+            }
+
+        }
+    }
+}
---
+++ Exercise8_Game/MainWindow.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Exercise8_Game
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int score;
+        double answer;
+        bool finished = false;
+        private string[] maths = new string[4] {"+","-","*","/" };
+        Random rand = new Random();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            GameStart();
+        }
+        
+
+        private async void GameStart()
+        {
+            score = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                finished = false;
+                answer = NextQuestion();
+                int j = 100;
+                do
+                {
+                    progressBar.Value = j--;
+                    await Task.Delay(80);
+                    if (finished) j=0;
+                } while (j > 0);
+
+
+            }
+        }
+
+        private double NextQuestion()
+        {
+            string selection = maths[rand.Next(0, 4)];
+            int x = rand.Next(0, 21);
+            int y = rand.Next(0, 21);
+
+            switch (selection)
+            {
+                case "+":
+                    tblkQuestions.Text = string.Format($"Add {x} and {y}");
+                    return x + y;
+                case "-":
+                    tblkQuestions.Text = string.Format($"Subtract {x} from {y}");
+                    return y - x;
+                case "*":
+                    tblkQuestions.Text = string.Format($"Multiply {x} by {y}");
+                    return x * y;
+                case "/":
+                    tblkQuestions.Text = string.Format($"Divide {x} by {y}");
+                    return x + y;
+            }
+            return 0;
+        }
+
+        private void ButtonSubmit_Click(object sender, RoutedEventArgs e)
+        {
+
+            double submittedAnswer = Convert.ToDouble(tbkAnswer.Text);
+
+            if (submittedAnswer == answer)
+            {
+                score++;
+                tblkScore.Text = string.Format($"Score:\t{score}");
+                finished = true;
+            }
+        }
+
+        private void TbkAnswer_GotFocus(object sender, RoutedEventArgs e)
+        {
+            tbkAnswer.Clear();
+        }
+    }
+}
